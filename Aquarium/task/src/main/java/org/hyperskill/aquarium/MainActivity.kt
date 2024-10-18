8257510f8ec72b4a5836988dbd7388e1c51d6beb
chinazablossom <chinazablossom7@gmail.com>
2024-10-18 21:30:01 +0100

    Stage 2

--- Aquarium/task/src/main/java/org/hyperskill/aquarium/MainActivity.kt
+++ Aquarium/task/src/main/java/org/hyperskill/aquarium/MainActivity.kt
@@ -1,24 +1,34 @@
 package org.hyperskill.aquarium
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.squareup.picasso.Callback
+import com.squareup.picasso.Picasso
 import org.hyperskill.aquarium.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        binding.apply {
-            val listOfAnimalsImages = intent.getSerializableExtra("imageAnimals") as? List<Int>
-            val listOfAnimalsNames = intent.getSerializableExtra("nameAnimals") as? List<String>
-            val listOfAnimalsDescription = intent.getSerializableExtra("descriptionAnimals") as? List<String>
-
-            binding.imageView.setImageResource(listOfAnimalsImages?.get(0) ?: R.drawable.image1)
-            binding.tvName.text = listOfAnimalsNames?.get(0) ?: "Koi Carp"
-            binding.tvDescription.text = listOfAnimalsDescription?.get(0) ?: ("These colorful, ornamental fish are a variety of the Amur carp. " +
+    private var initialPage = 0
+    private val listOfAnimalsImages by lazy {
+        intent.getSerializableExtra("imageAnimals") as? List<String> ?: listOf(
+            "https://ucarecdn.com/42045846-b968-4a88-81ec-df73bec4fcb7/",
+            "https://ucarecdn.com/5aa10eb3-fc49-4304-9057-adf1d29a9b4c/",
+            "https://ucarecdn.com/c5fd39b9-7690-4616-b7dc-d3f8da883146/"
+        )
+    }
+    private val listOfAnimalsNames by lazy {
+        intent.getSerializableExtra("nameAnimals") as? List<String> ?: listOf(
+            "Koi Carp",
+            "Spiny Dogfish",
+            "Kaluga"
+        )
+    }
+    private val listOfAnimalsDescriptions by lazy {
+        intent.getSerializableExtra("descriptionAnimals") as? List<String> ?: listOf(
+            //Koi Carp
+            "These colorful, ornamental fish are a variety of the Amur carp. " +
                     "They were originally found in Central Europe and Asia, " +
                     "but they’ve spread to many other parts of the world. " +
                     "Koi carp are popular with breeders, and there are currently over 100 varieties " +
@@ -27,9 +37,79 @@ class MainActivity : AppCompatActivity() {
                     "The average age of a koi carp can vary based on the part of the world it’s bred in. " +
                     "Carps bred outside of Japan have an average lifespan of around 15 years," +
                     " while carps bred in Japan can live 40 years or more. The oldest koi carp on record," +
-                    " which was a fish named Hanako, reportedly lived for 226 years!")
+                    " which was a fish named Hanako, reportedly lived for 226 years!",
 
+            // Spiny dogfish
+            "The spiny dogfish is a type of shark with venomous spines in front of its dorsal fins." +
+                    " Not only is it an aggressive hunter, but these fish are known to hunt in packs!" +
+                    " Like many shark species, these fish grow slowly, " +
+                    "and some females don’t reach full maturity until they’re over 30 years old.\n" +
+                    "\n" +
+                    "While the lifespan of the spiny dogfish is already impressive, " +
+                    "some fish live for far longer than average. " +
+                    "Spiny dogfish in the Pacific Ocean tend to live longer than fish in the Atlantic," +
+                    " with some fish living longer than 80 years. " +
+                    "Females tend to mature later than males, and they usually live longer too.",
 
+            //Kaluga
+            "Sometimes called the river beluga, the kaluga is a type of predatory sturgeon." +
+                    " While these fish spend the majority of their time in freshwater, " +
+                    "they’re also able to survive in salt water. " +
+                    "The kaluga is one of the world’s largest freshwater fish species and can grow to be more than 18 feet long, " +
+                    "with a weight of over 2,200 pounds.\n" +
+                    "\n" +
+                    "Kalugas are overfished, which has left the species vulnerable to extinction. " +
+                    "Although many kaluga are killed before they fully mature, " +
+                    "these fish have the potential to live very long lives. " +
+                    "One kaluga that was caught in China is estimated to be over 100 years old."
+        )
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        displayAnimal(initialPage)
+        binding.apply {
+            btnNext.setOnClickListener { navigateAnimal(1) }
+            btnPrevious.setOnClickListener { navigateAnimal(-1) }
         }
     }
+    private fun displayAnimal(page: Int) {
+        val imageUrl = listOfAnimalsImages[page]
+        val animalName = listOfAnimalsNames[page]
+        val animalDescription = listOfAnimalsDescriptions[page]
+
+        binding.apply {
+            progressBar.visibility = View.VISIBLE
+            tvName.text = animalName
+            tvDescription.text = animalDescription
+
+            Picasso.get()
+                .load(imageUrl)
+                .fit().centerInside()
+                .placeholder(R.drawable.placeholder)
+                .error(R.drawable.error)
+                .into(imageView, object : Callback {
+                    override fun onSuccess() {
+                        progressBar.visibility = View.GONE
+                    }
+
+                    override fun onError(e: Exception?) {
+                        progressBar.visibility = View.VISIBLE
+                        Toast.makeText(
+                            this@MainActivity,
+                            "Error! Check your network connectivity",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                })
+        }
+    }
+    private fun navigateAnimal(step: Int) {
+        initialPage = (initialPage + step + listOfAnimalsImages.size) % listOfAnimalsImages.size
+        displayAnimal(initialPage)
+    }
 }
+
+
